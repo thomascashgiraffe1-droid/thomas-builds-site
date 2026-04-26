9585e93224c3743e51c4350ca724bb1dcdce9970
thomascashgiraffe1-droid <thomascashgiraffe1@gmail.com>
2026-04-26 01:26:19 -0700

    Add files via upload

--- site-config.js
+++ site-config.js
@@ -5,8 +5,8 @@ window.THOMAS_BUILDS_CONFIG = {
   heroVideoUrl:
     "https://cdn.discordapp.com/attachments/1489756694434156585/1497780277240397935/Roblox-2026-04-26T02_03_08.364Z.mp4",
 
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://hbddkwlxoyxpivlvrfcx.supabase.co",
+  supabaseAnonKey: "sb_publishable_ts6TD4ItV7n1Jum8aJhWSA_UH3Z3-Ag",
 
   discordUrl: "",
   tiktokUrl: "",
